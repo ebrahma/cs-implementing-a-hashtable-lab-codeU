c2988db6060a803a11aaf8a74c407910013af8a0
ebrahma <ebrahma@gmail.com>
2016-08-02 23:59:55 -0500

    Done.

--- javacs-lab08/src/com/flatironschool/javacs/MyHashMap.java
+++ javacs-lab08/src/com/flatironschool/javacs/MyHashMap.java
@@ -41,7 +41,16 @@ public class MyHashMap<K, V> extends MyBetterMap<K, V> implements Map<K, V> {
 	 */
 	protected void rehash() {
         // TODO: fill this in.
-        throw new UnsupportedOperationException();
+		List<MyLinearMap<K, V>> oldMaps = maps;
+
+		int newK = maps.size() * 2;
+		makeMaps(newK);
+		
+		for (MyLinearMap<K, V> map: oldMaps) {
+			for (Map.Entry<K, V> entry: map.getEntries()) {
+				put(entry.getKey(), entry.getValue());
+			}
+		}
 	}
 
 	/**
